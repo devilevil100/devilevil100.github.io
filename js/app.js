af579d125c6d05f11cce009ac3e834a0ea30a00b
Keshav Kumar <devilevil100@gmail.com>
2020-10-27 18:12:26 +0530

    last commit ig

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-jQuery(document).ready(function($) {
-            $('.counter1').counterUp({
-                delay: 10,
-                time: 1000
-            });
-
-        });
 
 
 (function($, document, window){
